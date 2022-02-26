b480b5717ad594f0d223a66349bc9fb04651b3ce
unknown <1700132276@qq.com>
2022-02-26 18:26:54 +0800

    fix syntax errors

--- src/sig/aigis-sig/sig_aigis-sig_III.c
+++ src/sig/aigis-sig/sig_aigis-sig_III.c
@@ -38,6 +38,6 @@ OQS_API OQS_STATUS OQS_SIG_aigis_sig_param_iii_sign(uint8_t* signature, size_t* 
     return (OQS_STATUS)msig_sign(secret_key, message, message_len, signature, signature_len);
 }
 OQS_API OQS_STATUS OQS_SIG_aigis_sig_param_iii_verify(const uint8_t* message, size_t message_len, const uint8_t* signature, size_t signature_len, const uint8_t* public_key) {
-    return (OQS_STATUS)msig_sign(public_key, signature, signature_len, message, message_len);
+    return (OQS_STATUS)msig_verify(public_key, signature, signature_len, message, message_len);
 }
 #endif
